686929b6b48987e8fa2ae5bb3e77f258614b73bc
Kartik Pandey <kartikpandey2@gmail.com>
2019-04-23 15:22:10 +0530

    Updates

---
+++ precache-manifest.cad476eb27e0fd51255e596638e3ad35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd4e8664dba92f2caae95335d44709fa",
+    "url": "/calendar-app/static/media/plus.fd4e8664.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/calendar-app/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "ed7684dd8ce66205982a",
+    "url": "/calendar-app/static/js/runtime~main.1c49685a.js"
+  },
+  {
+    "revision": "0cc488125fc2a6c5ce5f",
+    "url": "/calendar-app/static/js/main.568aaf9a.chunk.js"
+  },
+  {
+    "revision": "ecd6d33e0a464e91a8c8",
+    "url": "/calendar-app/static/js/2.ae27aae4.chunk.js"
+  },
+  {
+    "revision": "0cc488125fc2a6c5ce5f",
+    "url": "/calendar-app/static/css/main.52be3766.chunk.css"
+  },
+  {
+    "revision": "ecd6d33e0a464e91a8c8",
+    "url": "/calendar-app/static/css/2.c4486cfd.chunk.css"
+  },
+  {
+    "revision": "29bea795207a75128adf8f43c9eb39f1",
+    "url": "/calendar-app/index.html"
+  }
+];
